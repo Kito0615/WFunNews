cd25db467e1fc0b17330daed55539a0a0d0296aa
Kito0615 <anar0615@sina.com>
2016-06-15 13:43:12 +0800

    Play video bug fixed

---
+++ Base/LLVideoViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLVideoViewController.h
+//  WFunNews
+//
+//  Created by AnarL on 16/6/14.
+//  Copyright © 2016年 AnarL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLVideoViewController : UIViewController
+
+@end
---
+++ Base/LLVideoViewController.m
@@ -0,0 +1,13 @@
+//
+//  LLVideoViewController.m
+//  WFunNews
+//
+//  Created by AnarL on 16/6/14.
+//  Copyright © 2016年 AnarL. All rights reserved.
+//
+
+#import "LLVideoViewController.h"
+
+@implementation LLVideoViewController
+
+@end
